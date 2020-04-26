0dc7f52e30a00656531085f7ce0f1e2978c757b3
alwayssadnessman <512545048@qq.com>
2020-04-26 16:46:25 +0800

    初步添加部分代码

---
+++ src/components/login/login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div id="bg-box">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login",
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+
+        },
+        mounted() {
+
+        }
+    }
+</script>
+
+<style scoped>
+  #bg-box{
+    width: 100%;
+    height: 100%;
+  }
+
+</style>
