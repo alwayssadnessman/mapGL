6b7c81923d92465c9b1bcaa1f842aaf441cada97
alwayssadnessman <512545048@qq.com>
2020-05-08 22:15:15 +0800

    上传整个地图编辑文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import index from '@/components/index/index';
 import login from '@/components/login/login';
+import mapGL from '@/components/mapGL/mapGL';
 
 Vue.use(Router);
 
@@ -8,8 +10,17 @@ const router = new Router({
   routes: [
     {
       path: '/',
+      name: '首页',
+      component: index
+    },
+    {
+      path: '/login',
       name: 'login',
       component: login
+    },{
+      path: '/map',
+      name: 'mapGL',
+      component: mapGL
     }
   ]
 })
@@ -17,7 +28,8 @@ router.beforeEach((to,form,next) => {
   if(to.path==='/login'){
     next()
   }else {
-
+    console.log(to)
+    next()
   }
 })
 export default router;
